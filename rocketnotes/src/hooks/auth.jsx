7851bbfe7968549cce1920643ef7538ae227f74c
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-05 11:33:59 -0300

    update profile functions

--- rocketnotes/src/hooks/auth.jsx
+++ rocketnotes/src/hooks/auth.jsx
@@ -36,6 +36,21 @@ function AuthProvider({ children }) {
     setData({})
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put("/users", user)
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      setData({ user, token: data.token })
+      alert("Perfil atualizado com sucesso!")
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Não foi possível atualizar o perfil.")
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token")
     const user = localStorage.getItem("@rocketnotes:user")
@@ -51,7 +66,14 @@ function AuthProvider({ children }) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        updateProfile,
+        user: data.user,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   )
