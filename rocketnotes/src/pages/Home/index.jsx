78936155a1a730c6ac94feafbea8f00d6bf5b412
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-06 17:22:36 -0300

    selected tags function

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,5 +1,9 @@
+import { useState, useEffect } from "react"
 import { FiPlus } from "react-icons/fi"
+
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+
+import { api } from "../../services/api"
 
 import { Note } from "../../components/Note"
 import { Input } from "../../components/Input"
@@ -8,6 +12,28 @@ import { Section } from "../../components/Section"
 import { ButtonText } from "../../components/ButtonText"
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter((tag) => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      setTagsSelected((prevState) => [...prevState, tagName])
+    }
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+    fetchTags()
+  }, [tagsSelected])
+
   return (
     <Container>
       <Brand>
@@ -18,14 +44,23 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" $isactive />
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            /* $isactive */
+            $isactive={tagsSelected.length === 0}
+          />
         </li>
-        <li>
-          <ButtonText title="React" />
-        </li>
-        <li>
-          <ButtonText title="Nodejs" />
-        </li>
+        {tags &&
+          tags.map((tag) => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                $isactive={tagsSelected.includes(tag.name)}
+              />
+            </li>
+          ))}
       </Menu>
 
       <Search>
