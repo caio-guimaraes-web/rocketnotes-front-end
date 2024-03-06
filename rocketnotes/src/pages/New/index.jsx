0d6634333d7e2bb12a452e1c911bcd5cd8757287
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-06 10:42:38 -0300

    remove tag function

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -33,6 +33,10 @@ export function New() {
     setNewTag("")
   }
 
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted))
+  }
+
   return (
     <Container>
       <Header />
@@ -67,7 +71,13 @@ export function New() {
           <Section title="Marcadores">
             <div className="tags">
               {tags.map((tag, index) => (
-                <NoteItem key={String(index)} value={tag} onClick={() => {}} />
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => {
+                    handleRemoveTag(tag)
+                  }}
+                />
               ))}
 
               <NoteItem
