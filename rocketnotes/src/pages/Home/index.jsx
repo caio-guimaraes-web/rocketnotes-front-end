9c90beae448883d5c63ba666f4b4a123d5545632
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-03-07 10:22:09 -0300

    filter tag functions

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -12,10 +12,16 @@ import { Section } from "../../components/Section"
 import { ButtonText } from "../../components/ButtonText"
 
 export function Home() {
+  const [search, setSearch] = useState("")
   const [tags, setTags] = useState([])
   const [tagsSelected, setTagsSelected] = useState([])
+  const [notes, setNotes] = useState([])
 
   function handleTagSelected(tagName) {
+    if (tagName === "all") {
+      return setTagsSelected([])
+    }
+
     const alreadySelected = tagsSelected.includes(tagName)
 
     if (alreadySelected) {
@@ -32,7 +38,17 @@ export function Home() {
       setTags(response.data)
     }
     fetchTags()
-  }, [tagsSelected])
+  }, [])
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(
+        `/notes?title=${search}&tags=${tagsSelected}`
+      )
+      setNotes(response.data)
+    }
+    fetchNotes()
+  }, [tagsSelected, search])
 
   return (
     <Container>
@@ -64,20 +80,19 @@ export function Home() {
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" />
+        <Input
+          placeholder="Pesquisar pelo título"
+          onChange={() => {
+            setSearch(e.target.value)
+          }}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note
-            data={{
-              title: "React",
-              tags: [
-                { id: "1", name: "react" },
-                { id: "2", name: "rocketseat" },
-              ],
-            }}
-          />
+          {notes.map((note) => (
+            <Note key={String(note.id)} data={note} />
+          ))}
         </Section>
       </Content>
 
